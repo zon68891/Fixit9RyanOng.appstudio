b2b272386d1cec6e8afe401fcc52a99d3cd22b3f
Ryan <zon68891@creighton.edu>
2019-10-01 16:04:45 -0500

    CheckMembership

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,17 @@
 /*
+var memberName = ""
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+var membershipNew = []
+
+btnMembershipSubmit.onclick=function(){
+var memberName = inptFirstName.value
+  if (members.indexOf(memberName) > -1) {
+ lblVerify.value = `Welcome to the app, ${inptFirstName.value}`
+ membershipNew.push(inptFirstName.value)
+  }else{
+ lblVerify.value = `Your membership information is not recognized.`
+    inptFirstName.value = ""
+    }
+}
 */
 
